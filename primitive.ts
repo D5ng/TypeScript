81d0b23ec5b71a55e966bb8b08738cd072eb5523
d5ng <dong5ffice@gmail.com>
2023-04-10 23:32:02 +0900

    원시타입

---
+++ primitive.ts
@@ -0,0 +1,19 @@
+/**
+ * ! 타입 표기
+ * ! 식별자의 값 뒤에 : 를 붙여 타입을 명시적으로 표기하는것을 말한다.
+ */
+
+/**
+ * ! 타입 추론
+ * ! 할당된 값을 보고 타입스크립트가 타입을 추론한다. 한번 값을 지정한뒤에는 바꿀 수 없다.
+ */
+
+// 타입 표기로 작성하기.
+const numberAnnotation: number = 5;
+const stringAnnotation: string = '문자열';
+const booleanAnootation: boolean = true;
+
+// 타입 추론
+const numberInterface = 5;
+const stringInterface = '문자열';
+const booleanInterface = true;
