0b931c5cbfcbce25183d929d303d94674b5191b6
d5ng <dong5ffice@gmail.com>
2023-04-11 15:38:35 +0900

    never 타입

---
+++ never.js
@@ -0,0 +1,12 @@
+/**
+ * ! 네버 타입
+ * ! never는 일반적으로 함수의 리턴 타입으로 사용됩니다. 함수의 리턴 타입으로 never가 사용될 경우,
+ * ! 항상 오류를 출력하거나 리턴 값을 절대로 내보내지 않음을 의미합니다. 이는 무한 루프(loop)에 빠지는 것과 같습니다.
+ */
+function generateError(message, errorCode) {
+    throw {
+        message: message,
+        errorCode: errorCode,
+    };
+}
+generateError("An Error occurred", 500);
