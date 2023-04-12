e842b2e37881790b29ceed438ce95356e268193b
d5ng <dong5ffice@gmail.com>
2023-04-12 12:50:22 +0900

    class inherit

--- class.ts
+++ class.ts
@@ -1,11 +1,6 @@
 class Department {
-  // private id: string;
-  // name: string;
   private employees: string[] = [];
-  constructor(private readonly id: string, public name: string) {
-    this.id = id;
-    this.name = name;
-  }
+  constructor(private readonly id: string, public name: string) {}
 
   describe(this: Department) {
     console.log(`Department: (${this.id}) ${this.name}`);
@@ -21,10 +16,34 @@ class Department {
   }
 }
 
-const account = new Department("d1", "Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
-account.describe();
-account.addEmployee("DongHyun");
-account.addEmployee("SuZy");
+class AccountingDepartment extends Department {
+  constructor(id: string, public reports: string[]) {
+    super(id, "Accounting");
+    this.reports = reports;
+  }
 
-account.printEmployeeInformation();
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Dongs"]);
+it.describe();
+it.addEmployee("DongHyun");
+it.addEmployee("SuZy");
+it.printEmployeeInformation();
+
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Someting went wrong!");
+accounting.printReports();
