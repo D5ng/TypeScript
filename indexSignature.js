27736969fe2f60e1728b63963303560622c99090
d5ng <dong5ffice@gmail.com>
2023-04-14 18:54:32 +0900

    인덱스 시그니처

--- indexSignature.js
+++ indexSignature.js
@@ -7,3 +7,17 @@ var loginError = {
     statusError: 400,
     messageError: "400 Bad Request",
 };
+var shoppingList = {
+    iMac: 130,
+    mac: 200,
+    trackpad: 40,
+    airpod: 50,
+    iPad: 130,
+};
+function totalSalary(shoppingList) {
+    var result = 0;
+    for (var item in shoppingList)
+        result += shoppingList[item];
+    return result;
+}
+console.log(totalSalary(shoppingList));
--- indexSignature.ts
+++ indexSignature.ts
@@ -13,3 +13,23 @@ const loginError: ErrorContainer = {
   statusError: 400,
   messageError: "400 Bad Request",
 };
+
+interface ShoopingList {
+  [key: string]: number;
+}
+
+const shoppingList: ShoopingList = {
+  iMac: 130,
+  mac: 200,
+  trackpad: 40,
+  airpod: 50,
+  iPad: 130,
+};
+
+function totalSalary(shoppingList: ShoopingList) {
+  let result = 0;
+  for (const item in shoppingList) result += shoppingList[item];
+  return result;
+}
+
+console.log(totalSalary(shoppingList));
