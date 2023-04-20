93a223f16738b0fe9dd61e357890e7b3bac69855
d5ng <dong5ffice@gmail.com>
2023-04-20 10:26:02 +0900

    클래스 데코레이터에서 클래스 반환하기

--- decorator/decorator.js
+++ decorator/decorator.js
@@ -5,35 +5,37 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Log2(target, name, descriptor) {
-    console.log("Access Decorator!");
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.dir(constructor);
+    };
 }
-function Log3(target, prop, descriptor) {
-    console.log("Method Decorator");
-    console.log(target);
-    console.log(prop);
-    console.log(descriptor);
+function WithTemplate(template, hookId) {
+    console.log(`Template Factory`);
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log("Template Constructor");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = this.name;
+                }
+            }
+        };
+    };
 }
-class Product {
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error("Invalid Price!!");
-        }
+let Person = class Person {
+    constructor() {
+        this.name = "Dongs";
+        console.log("creating person object...");
     }
-    constructor(title, price) {
-        this.title = title;
-        this._price = price;
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
-    }
-}
-__decorate([
-    Log3
-], Product.prototype, "getPriceWithTax", null);
+};
+Person = __decorate([
+    Logger("LOGGING - PERSON"),
+    WithTemplate("<h1>My Person Object</h1>", "app")
+], Person);
+const person = new Person();
+console.log(person);
--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -4,77 +4,90 @@
  */
 
 // ! 클래스 데코레이터
-// function Logger(logString: string) {
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.dir(constructor);
-//   };
-// }
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.dir(constructor);
+  };
+}
 
-// function WithTemplate(template: string, hookId: string) {
-//   console.log(`Template Factory`);
-//   return function (constructor: any) {
-//     console.log("Template Constructor");
-//     const hookEl = document.getElementById(hookId)!;
-//     const p = new constructor();
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector("h1")!.textContent = p.name;
-//     }
-//   };
-// }
+function WithTemplate(template: string, hookId: string) {
+  console.log(`Template Factory`);
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Template Constructor");
+        const hookEl = document.getElementById(hookId)!;
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
-// @Logger("LOGGING - PERSON")
-// @WithTemplate("<h1>My Person Object</h1>", "app")
-// class Person {
-//   name = "Dongs";
-//   constructor() {
-//     console.log("creating person object...");
-//   }
-// }
-// const person = new Person();
-// console.log(person);
+@Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Dongs";
+  constructor() {
+    console.log("creating person object...");
+  }
+}
+const person = new Person();
+console.log(person);
 
 // function Log(target: any, propertyName: string | Symbol) {
 //   console.log("Property decorator");
 //   console.log(target, propertyName);
 // }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log("Access Decorator!");
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
+// function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+//   console.log("Access Decorator!");
+//   console.log(target);
+//   console.log(name);
+//   console.log(descriptor);
+// }
 
-function Log3(target: any, prop: string, descriptor: PropertyDescriptor) {
-  console.log("Method Decorator");
-  console.log(target);
-  console.log(prop);
-  console.log(descriptor);
-}
+// function Log3(target: any, prop: string, descriptor: PropertyDescriptor) {
+//   console.log("Method Decorator");
+//   console.log(target);
+//   console.log(prop);
+//   console.log(descriptor);
+// }
 
-class Product {
-  // @Log
-  title: string;
-  private _price: number;
+// function Log4(target: any, name: string, parameterIndex: number) {
+//   console.log("Parameter Decorator");
+//   console.log(target);
+//   console.log(name);
+//   console.log(parameterIndex);
+// }
 
-  // @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error("Invalid Price!!");
-    }
-  }
+// class Product {
+//   // @Log
+//   title: string;
+//   private _price: number;
 
-  constructor(title: string, price: number) {
-    this.title = title;
-    this._price = price;
-  }
+//   // @Log2
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Invalid Price!!");
+//     }
+//   }
 
-  @Log3
-  getPriceWithTax(tax: number) {
-    return this._price * (1 + tax);
-  }
-}
+//   constructor(title: string, price: number) {
+//     this.title = title;
+//     this._price = price;
+//   }
+
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
